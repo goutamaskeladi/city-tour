0e744adfae15ec2d0ddc0023f2b1424e1011eb6b
Goutam A S <goutamas.keladi@gmail.com>
2019-09-12 13:01:34 +0530

    Modified Menu Links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,11 @@ import Logo from '../../logo.png'
 export default function Navbar() {
     return (
         <nav className="navbar">
-           <img src={Logo} alt="City Tour Logo"/>
+           <img src={Logo} alt="City Tour Logo" onClick={() => location.reload()}/>
            <ul className="nav-links">
               <li>
-                  <a href="/" className="nav-link">Home</a>
-                  <a href="/about" className="nav-link">About</a>
-                  <a href="/services" className="nav-link">Services</a>
+                  <a href="/" className="nav-link" onClick={() => location.reload()}>Home</a>
+                  <a href="/explore" className="nav-link">Explore</a>
                   <a href="/contact" className="nav-link active">Contact</a>
               </li>
            </ul>
